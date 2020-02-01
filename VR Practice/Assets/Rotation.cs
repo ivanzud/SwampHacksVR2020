82b1127056b3307f3e05ef0893ef80af45e0affe
jbendezu01 <54551739+jbendezu01@users.noreply.github.com>
2020-02-01 16:06:06 -0500

    Starting Project
    Cannon rotates

---
+++ VR Practice/Assets/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.right * speed);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.left * speed);
+
+    }
+}
