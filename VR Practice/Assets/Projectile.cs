99f7d56c2c89f713f801bbbfb3c19d75b7eca658
jbendezu01 <54551739+jbendezu01@users.noreply.github.com>
2020-02-01 20:45:02 -0500

    UI
    UI has been started

--- VR Practice/Assets/Projectile.cs
+++ VR Practice/Assets/Projectile.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public float speed = 1.0f;
+    public Rigidbody bulletPrefabs;
+    public LayerMask layer;
+    private Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-
-        }
-
 
     }
+
+    void LaunchProjectile()
+    {
+
+    }
+
+    Vector3 Velocity(Vector3 target, Vector3 origin, float time)
+    {
+        Vector3 distance = target - origin;
+        Vector3 distanceXZ = distance;
+        distance.y = 0;
+
+        float Sy = distance.y;
+        float Sxz = distanceXZ.magnitude;
+
+        float Vxz = Sxz / time;
+        float Vy = Sy / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
+
+        Vector3 result = distanceXZ.normalized;
+        result *= Vxz;
+        result.y = Vy;
+
+        return result;
+    }
+
 }
--- VR Practice/Assets/Rotation.cs
+++ VR Practice/Assets/Rotation.cs
@@ -15,10 +15,18 @@ public class Rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.right * speed);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(Vector3.left * speed);
+        Debug.Log(transform.rotation.x);
 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.rotation.x < 0.7)
+                transform.Rotate(Vector3.right * speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.rotation.x > -0.7)
+                transform.Rotate(Vector3.left * speed);
+        }
     }
 }
